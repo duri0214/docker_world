0fe211315be872627557d50f81e053701ab1fb87
yoshi <yoshitakaOkada0214@gmail.com>
2024-10-05 12:09:34 +0900

    fix: create sample

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+// frontend/src/pages/index.tsx
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Home
